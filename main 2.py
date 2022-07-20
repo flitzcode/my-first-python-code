7c7389bd9562885d1dc18076a05d55d9fbafe847
flitzcode <muchamadfaiz@gmail.com>
2022-07-20 11:41:33 +0700

    koding budi update cuy

--- main 2.py
+++ main 2.py
@@ -14,17 +14,16 @@ print("Dan mulai berbelanja")
 
 # Percabangan
 jumlah_botol_susu = 173
-jumlah_telur = 1573
+jumlah_telur = 180
 print(f"Jumlah Botol susu {jumlah_botol_susu} btl")
 print(f"Jumlah Telur {jumlah_telur} butir")
 
 if jumlah_botol_susu > 0:
     print("Budi mengecek harganya, dan ternyata uangnya cukup")
-    if jumlah_telur == 0:
-       print("Budi membeli 1 botol susu")
+    if jumlah_telur > 0:
+       print("Budi membeli 1 botol susu & 6 butir telur")
     else:
-        print("Budi membeli 6 botol susu")
-
+        print("Budi membeli 1 botol susu")
 
 else:
     print("Budi tidak jadi membeli 1 botol susu")
