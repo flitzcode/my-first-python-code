7cc86c077210925b4579f02ddaf1d11876a74d64
flitzcode <muchamadfaiz@gmail.com>
2022-07-20 10:37:18 +0700

    koding budi update

--- main 2.py
+++ main 2.py
@@ -11,3 +11,15 @@ print('Budi menjawab,"Baik, apa yang harus saya lakukan di toko?"')
 print('Ibu menjawab,"Beli satu botol susu, dan jika ada telor beli 6"')
 print("Maka Budi berangkat ke toko")
 print("Dan mulai berbelanja")
+
+# Percabangan
+jumlah_botol_susu = 0
+
+if jumlah_botol_susu > 0:
+    print("Budi mengecek harganya, dan ternyata uangnya cukup")
+    print("Budi membeli 1 botol susu")
+else:
+    print("Budi tidak jadi membeli 1 botol susu")
+
+print("Budi pulang ke rumah")
+print("Menyampaikan hasilnya kepada ibu")
