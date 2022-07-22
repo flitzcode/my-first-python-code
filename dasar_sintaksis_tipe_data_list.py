b25038206afe8da155236b7e7ff96e4d0559ed82
flitzcode <muchamadfaiz@gmail.com>
2022-07-22 22:05:52 +0700

    tipe data list

--- dasar_sintaksis_tipe_data_list.py
+++ dasar_sintaksis_tipe_data_list.py
@@ -19,9 +19,40 @@ daftar_buku = [1, 'Kenji Volume 2', -313, '3.14']
 for i in range(0, len(daftar_buku)):
     print(daftar_buku[i])
 
-print('kembalikan nilai awal daftar_buku')
+print('\nkembalikan nilai awal daftar_buku')
 daftar_buku = ['Seven Habits', 'How to Influence People', 'First Things First', '4DX']
-print('Tambahkan 1 buku baru')
+print('\nTambahkan 1 buku baru')
 daftar_buku.append('Dunia Matematik Kelas 5')
 for i in range(0, len(daftar_buku)):
     print(daftar_buku[i])
+
+print('\nClear List')
+daftar_buku.clear()
+for i in range(0, len(daftar_buku)):
+    print(daftar_buku[i])
+
+print('\nGanti elemen pertama')
+daftar_buku = ['Seven Habits', 'How to Influence People', 'First Things First', '4DX']
+daftar_buku[0] = 'Eight Habits'
+for i in range (0, len(daftar_buku)):
+    print(daftar_buku[i])
+
+print('\nAmbil elemen ke-2')
+buku = daftar_buku.pop(1)
+for i in range(0,len(daftar_buku)):
+    print(daftar_buku[i])
+
+print('\nBuku yang diambil tadi')
+print(buku)
+
+print('\nPop')
+daftar_buku.pop()
+for i in range(0,len(daftar_buku)):
+    print(daftar_buku[i])
+
+print('\nPop -1')
+daftar_buku = ['Seven Habits', 'How to Influence People', 'First Things First', '4DX']
+daftar_buku.pop(-1)
+for i in range(0,len(daftar_buku)):
+    print(daftar_buku[i])
+
