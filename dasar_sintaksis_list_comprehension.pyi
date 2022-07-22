0b9b9c042d11106fc315ce59c5969f705deb25df
flitzcode <muchamadfaiz@gmail.com>
2022-07-22 23:06:01 +0700

    list comprehension

---
+++ dasar_sintaksis_list_comprehension.pyi
@@ -0,0 +1,23 @@
+print('\nPerintah del')
+daftar_buku = ['Seven Habits', 'How to Influence People', 'First Things First', '4DX']
+del daftar_buku [0]
+for i in range(0,len(daftar_buku)):
+    print(daftar_buku[i])
+
+print('\nPerintah del dengan list comprehension')
+daftar_buku = ['Seven Habits', 'How to Influence People', 'First Things First', '4DX']
+del daftar_buku [:]
+for i in range(0,len(daftar_buku)):
+    print(daftar_buku[i])
+
+print('\nPerintah del dengan list comprehension start')
+daftar_buku = ['Seven Habits', 'How to Influence People', 'First Things First', '4DX']
+del daftar_buku [0:-2] #STAR:END
+for i in range(0,len(daftar_buku)):
+    print(daftar_buku[i])
+
+print('\nPerintah del dengan list comprehension start dan step')
+daftar_buku = ['Seven Habits', 'How to Influence People', 'First Things First', '4DX']
+del daftar_buku [0::2] #START:END:STEP
+for i in range(0,len(daftar_buku)):
+    print(daftar_buku[i])
