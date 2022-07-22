5c6d3421730c1448b18b0caed09d5d9b4c646447
flitzcode <muchamadfaiz@gmail.com>
2022-07-22 23:30:06 +0700

    list comprehension

--- dasar_sintaksi_list_comprehension.py
+++ dasar_sintaksi_list_comprehension.py
@@ -22,3 +22,20 @@ daftar_buku = ['Seven Habits', 'How to Influence People', 'First Things First', 
 del daftar_buku [0::2] #START:END:STEP
 for i in range(0,len(daftar_buku)):
     print(daftar_buku[i])
+
+print('\nMembuat list baru')
+daftar_buku = ['Seven Habits', 'How to Influence People', 'First Things First', '4DX']
+daftar_buku_baru = daftar_buku[:]
+for i in range(0,len(daftar_buku)):
+    print(daftar_buku[i])
+
+print('\nMembuat list baru')
+del daftar_buku[:]
+for i in range(0,len(daftar_buku_baru)):
+    print(daftar_buku_baru[i])
+
+print('\nMembuat list baru dengan comprehension:ganjil')
+daftar_buku = ['1 Seven Habits', '2 How to Influence People', '3 First Things First', '4 4DX']
+daftar_buku_baru = daftar_buku[0::2]
+for i in range(0, len(daftar_buku_baru)):
+    print(daftar_buku_baru[i])
