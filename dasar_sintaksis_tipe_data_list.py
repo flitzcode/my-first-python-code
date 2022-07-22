b653faf526300a55c0c456d46137a887bbfb97b8
flitzcode <muchamadfaiz@gmail.com>
2022-07-22 21:11:42 +0700

    tipe data list

---
+++ dasar_sintaksis_tipe_data_list.py
@@ -0,0 +1,13 @@
+daftar_buku = ['Seven Habits', 'How to Influence People', 'First Things First']
+print('Tampilkan variable daftar_buku')
+print(daftar_buku)
+
+print('\nProses semua dengan for in')
+for buku in daftar_buku:
+    print(buku)
+
+print('\nTampilkan isi daftar_buku pada indeks tertentu')
+print(daftar_buku[0])
+print(daftar_buku[2])
+
+
